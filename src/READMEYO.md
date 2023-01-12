376d866a6048e15d110bc447a1963e3b63c40869
AnaDaniela <danielaandrade66@gmail.com>
2023-01-12 15:48:18 -0600

    last changes before update.

---
+++ src/READMEYO.md
@@ -0,0 +1,34 @@
+# **Card validation**
+
+## **¿De qué se trata mi proyecto?**
+ 
+Bienvenid@ a Museo de barcos y armas de Campeche. Una página que simulá la compra de la entrada al Museo.
+Está pensada para personas que se conectan y prefieren realizar y anticipar sus compras desde el ordenador. 
+
+Este es mi proyecto hasta ahora 04 de agosto de 2022. No está concluido el diseño, pero aún tengo esperanza de actualizar pronto la sección de lado derecha y la header.![Finalproyect](./assets/ImagenFinal.png)
+
+## **Inspiración**
+En el mundo existen diferentes Museos fenómenales, sin embargo, en cuanto a la experiencia en línea, son muchos los parámetros con los que ahora tienen que cumplir, por lo que algunos se quedan resagados y sus visitas disminuyen. Tal es el caso del [Museo de barcos y armas de Campeche.](https://es.wikipedia.org/wiki/Museo_de_Barcos_y_Armas_Fuerte_de_San_Jos%C3%A9_el_Alto) Un lugar espectacular que me inspiró, en la creación de está nueva experiencia; al cuál, le caería bastante bien, tener una venta de boletos en línea y una suscribción de los contenidos que pueden hacerse desde ahí.
+
+## **Proceso creativo**
+Luego de definir un objetivo y hacía qué tipo de producto apuntaba mi proyecto, me concentré en elegir una gama de colores. Para ello utilicé la página [Colors.](https://coolors.co/palette/8ecae6-219ebc-023047-ffb703-fb8500) Cuya gama también se aprecía aquí:
+![Colores](./assets/paletadecolores.jpg)
+
+Enseguida, realice un _MoodBoard_ que aquí anexo, el cual contiene los colores que quería usar y me sirvieron de inspiración durante el proyecto.
+ ![MOODBOARD](./assets/MOODBOARD.JPG.png)
+
+ Ya con los colores y la inspiración en mano, seguí los siguientes pasos: un boceto a papel y lápiz de lo que quería hacer que anexo enseguida, un boceto en [slides.google](https://docs.google.com/presentation/d/1sDVu_timhxPph-VirjX3bmctdifTE3d7HKtbNLuVqs8/edit#slide=id.g13cd2a1d99f_0_258)y finalmente, un primer intento con la plataforma [figma.](https://www.figma.com/file/r9xrekkKSeYn6nvGbiUFhx/Untitled?node-id=0%3A1).
+ ![SKETCH](./assets/sketchpaper%20andpencil.jpg)
+
+ ## ¿Para quién es mi sitio?
+ Uno de mis temas preferidos es el museo como objeto. ¿A qué me refiero? A pensar en el discurso que se construye dentro de las exposiciones museográficas, tanto como dentro de los elementos comunicativos que producen. Así entonces, las páginas web que utilizan son ahora herramientas para la divulgación. Esto hace que me parezca un tema super interesante, y que se puede abordar perfecto en estos proyectos. 
+ Este sitio esta diseñado para que usuarios acudan a la "Museo de armas y barcos de Campeche" para comprar sus tickets e poder suscribirse a la página del museo. A cambio de sostener una revista digital. 
+ Esto resulta en un posible intercambio con los objetos dentro del mismo museo y compartir la historia que guarda.
+
+ ## Feedback que recibí:
+ Lo primero que me recomendaron fue: "enfocate en el objetivo principal" porque tenía la idea de simular toda la compra. Algo que fui comprendiendo mientras avanzaba, era muy ambicioso. 
+ Más adelante me sugirieron que avanzara con el diseño de css porque primero, programé todo en javascript.
+ Que me enfocará sólo en mostrar que la tarjeta era "válida".
+ Que le pusiera otro color de letra a "tarjeta válida" o "tarjeta inválida".
+
+
--- src/index.js
+++ src/index.js
@@ -1,72 +1,65 @@
-import validator from './validator.js';
+import validator from './validator.js'; //de esta forma se exporta validator 
 
-//para que tenga que llenar el formulario 
-const sendFormBoton = document.getElementById("sendForm")
-sendFormBoton.addEventListener("click",validarFormulario)
+const sendFormBoton = document.getElementById("sendForm") //constante para tomar el elemento id "sendFomr"
+sendFormBoton.addEventListener("click",validarFormulario) //evento addEventListener para el botón con id sendFormBoton, asociado a la función validarFormulario
 
-
-//HACER QUE CAMBIEN LOS DIGITOS
-
-document.getElementById ("numTarjeta").addEventListener("keyup",getMask)
-
-
-//Funciara que entren a validator sólo números y de 16 dígitos
-
+//"addEventListener" para que que entren a validator sólo números y sólo 16 dígitos
 const boton = document.getElementById ("botonValidar")
 boton.addEventListener("click",getCardNumb)
 
-
 //FUNCIONES 
-function validarFormulario(){
-    const entrada = ["firstName","lastName","email"];
-    const etiqueta = ["primerNombre","apellido","correo"];
-    let asteriscos = 0;  // conyador de asteriscos
+function validarFormulario(){ //función sin parametros
+    const entrada = ["firstName","lastName","email"];//arreglo con id's de input en html
+    const etiqueta = ["primerNombre","apellido","correo"]; //arreglo con id's de labels html 
+    let asteriscos = 0;  // contador de asteriscos
+    
+    //ciclo for loop to check if any field of the form is empty.
     for(let i=0; i<entrada.length;i++){
         let ent = document.getElementById(entrada[i]).value;
-        if (ent.length==0){
-            document.getElementById(etiqueta[i]).innerHTML= "*";
-            asteriscos = asteriscos + 1;
-        }
+        if (ent.length==0){ //si la longitud de ent es cero, entonces
+            document.getElementById(etiqueta[i]).innerHTML= "*"; //asigna a la etiqueta con índice i un *
+            asteriscos = asteriscos + 1; //suma uno en el contador de asteriscos 
+        } //cierra el if
+    } //cierra for loop
+    if(asteriscos != 0){ //si el contador de asteriscos es diferente de cero
+        ////imprime un mensaje en la etiqueta "asteriscos" de html
+        document.getElementById("asteriscos").innerHTML= "<h4>*Los campos marcados son obligatorios*</h4>";
+    } //cierra if
+    let tipo_tarjeta = document.getElementById("typeTarjet").value; //a tipo_tarjeta asigna el valor que tiene el input typeTarjet en html
+    if(tipo_tarjeta == "select"){ //sí "tipo_tarjeta es igual a "select"
+        document.getElementById("tipoTarjeta").innerHTML= "<h4>*Selecciona un tipo de tarjeta*</h4>"; //en el label "tipo tarjeta imprime un mensaje"
+        document.getElementById("asteriscos").innerHTML= "<h4>*Los campos marcados son obligatorios*</h4>"; //en el label "asteriscos" imprime 
     }
-    if(asteriscos != 0){
-        document.getElementById("asteriscos").innerHTML= "* Los campos marcados son obligatorios *";
-    }
-    let tipo_tarjeta = document.getElementById("typeTarjet").value;
-    if(tipo_tarjeta == "select"){
-        document.getElementById("tipoTarjeta").innerHTML= "* Selecciona un tipo de tarjeta";
-        document.getElementById("asteriscos").innerHTML= "* Los campos marcados son obligatorios *";
-    }
-    if(asteriscos==0 && tipo_tarjeta!="select"){
-        return true
-    } else if(asteriscos!=0 || tipo_tarjeta=="select"){
-        return false
-    }
-}
+    if(asteriscos==0 && tipo_tarjeta!="select"){ //si el formulario está lleno && "tipoTarjeta" es diferente de "select"
+        return true //regresa un verdadero 
+    } else if(asteriscos!=0 || tipo_tarjeta=="select"){ //si "asteriscos es diferente de cero ||"o" "tipo_tarjeta" igual a "select"
+        return false //regresa falso
+    }//fin del if
+}//fin de la función validarFormulario
 
-function getCardNumb(){
-    const numStr = document.getElementById ("numTarjeta").value; //guarda la cadena introducidan por el usuario
+function getCardNumb(){ //función sin parametros
+    const numStr = document.getElementById ("numTarjeta").value; //guarda la cadena introducida por el usuario
     let numArr = numStr.split(""); // tranforma en un arreglo la cadena numStr
     if (numArr.length==16) { // Si la longitud del arreglo es igual a 16, ENTONCES
 
         for (let i=0; i<numArr.lenght; i++){ // PARA i desde 0; hasta 16; con incrmento de 1
-            if(isNaN(numArr[i])){
+            if(isNaN(numArr[i])){ //si "numArr" que es el arreglo con que irá iterando, no es un numero 
+                //regresará este mensaje de "solo lee caracteres numéricos"
                 document.getElementById("justNumbers").innerHTML="Este algortimo sólo lee caracteres numéricos"
-            }
+            }//va revisar todos los elementos dentro de este if
+        } //sale del ciclo 
+
+        let arre = validator.isValid(numStr) //variable para tomar el numStr ya validado por validator.isValid
+        if (arre==true) { //sí arre es igual a true
+            document.getElementById("justNumbers").innerHTML = "<h5>Tarjeta válida</h5>" //toma id "justnumbers" del html y muestra
+            //PARA HACER QUE CAMBIEN LOS DIGITOS POR #'s EN LA CAJA DE ENTRADA DE TEXTO
+            const maskifyValue = validator.maskify(numStr); //variable maskifyValue que será igual a validator.maskify
+            document.getElementById("numTarjeta").value = maskifyValue; //le asigno el valor de maskifyValue al id "numTarjeta"
+        }else{
+            document.getElementById("justNumbers").innerHTML = "<h4>Tarjeta inválida</h4>" //si no es igual a true entonces muestra "tarjeta inválida"
         }
-
-        let arre = validator.isValid(numStr);
-        document.getElementById("justNumbers").innerHTML = arre;
-        let mask = validator.maskify(numStr);
-        document.getElementById("justSixteen").innerHTML = mask;
-
     } 
     else{
         document.getElementById("justSixteen").innerHTML="Tienes que escribir 16 dígitos"
     } 
 }    
-
-function getMask(){
-    let str = document.getElementById("numTarjeta").value
-    let masked = validator.maskify(str)
-    document.getElementById("justSixteen").innerHTML= masked
-}
